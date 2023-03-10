f4d8d107b961dc4e6dc9116ad2dafb629996e290
Prajol Shrestha <prajolstha9@gmail.com>
2023-03-10 16:49:52 +0100

    Update tf2_0_autoregressive_model.py

--- tf2_0_autoregressive_model.py
+++ tf2_0_autoregressive_model.py
@@ -51,3 +51,49 @@ plt.plot(r.history['loss'], label='loss')
 plt.plot(r.history['val_loss'], label='val_loss')
 plt.legend()
 
+# Wrong forecast using true targets
+
+validation_target = Y[-N//2:] #use second half of Y
+validation_predictions = [] # empty list
+
+#index of first validation input
+i= -N//2
+
+while len(validation_predictions) < len(validation_target):
+     p = model.predict(X[i].reshape(1,-1))[0,0] 
+     #X[i].reshape(1,-1) =====> 1xT (1 samples and T features)
+     #again model.predict() returns N x K output (N samples and K output node)
+     # 1x1 array ma index [0,0]------> scalar value 
+     
+     i += 1
+
+     #update the predictions list
+     validation_predictions.append(p)
+
+plt.plot(validation_target, label='forecast Target')
+plt.plot(validation_predictions, label = 'forecast Prediction')
+plt.legend()
+
+###Forecast future values (use only self-predictions for making future predictions)
+
+validation_target = Y[-N//2:] #use second half of Y
+validation_predictions = [] # empty list
+
+#last train input
+last_x = X[-N//2] #first input vector #1D array of length T #we wont use any new value from actual dataset
+
+while len(validation_predictions) < len(validation_target):
+     p = model.predict(last_x.reshape(1,-1))[0,0] 
+     #again model.predict() returns N x K output (N samples and K output node)
+     # 1x1 array ma index [0,0]------> scalar value 
+     
+     #update the predictions list
+     validation_predictions.append(p)
+
+     #make the new input
+     last_x = np.roll(last_x, -1) # roll(last_x, -1) helps to shift everything to one step to left
+     last_x[-1] = p
+
+plt.plot(validation_target, label='forecast Target')
+plt.plot(validation_predictions, label = 'forecast Prediction')
+plt.legend()
